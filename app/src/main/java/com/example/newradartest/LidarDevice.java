0fb8f276a5ea316bc01c06ed06d1ae3d195f4c8e
Leon <1342723111@qq.com>
2021-04-02 20:50:58 +0800

    sth12

--- app/src/main/java/com/example/newradartest/LidarDevice.java
+++ app/src/main/java/com/example/newradartest/LidarDevice.java
@@ -236,6 +236,7 @@ public class LidarDevice {
             }
 
             setConnected(false);
+            setStreamed(false);
             Log.i(TAG, "成功断开设备连接");
 
         } catch (IOException e) {
